d7477faea46e184af4c59910dcb116fdb1b51218
Matt Lehrer <lehrerm@gmail.com>
2021-04-05 15:50:57 -0700

    fix: blockquote opening quote in light mode match dark mode style

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,50 @@ module.exports = {
       },
       typography: (theme) => ({
         DEFAULT: {
-          css: {
-            h2: {
-              fontWeight: '500',
+          css: [
+            {
+              h2: {
+                fontWeight: '500',
+              },
+              h3: {
+                fontWeight: '500',
+              },
+              a: {
+                textDecoration: 'none',
+                color: 'inherit',
+                fontWeight: 'inherit',
+              },
+              'code::before': false,
+              'code::after': false,
+              code: {
+                backgroundColor: theme('colors.gray.200'),
+                padding: '.25em',
+              },
+              blockquote: {
+                fontWeight: '300',
+                fontStyle: 'italic',
+                color: theme('colors.gray.700'),
+                borderLeftColor: theme('colors.gray.600'),
+                // hack - not sure why single quote is showing up on second blockquotes
+                quotes: '"\\201C""\\201D""\\201C""\\2019"',
+              },
+              'blockquote p:first-of-type::before': {
+                // content: 'open-quote',
+                'font-size': '5em',
+                opacity: '30%',
+                'line-height': '0.1em',
+                'margin-left': '-0.2em',
+                'margin-right': '0.15em',
+                'vertical-align': '-0.4em',
+              },
+              'blockquote p:last-of-type::after': {
+                content: 'none',
+                // 'font-size': '4em',
+                // opacity: '50%',
+                // 'line-height': '0.1em',
+              },
             },
-            h3: {
-              fontWeight: '500',
-            },
-            a: {
-              textDecoration: 'none',
-              color: 'inherit',
-              fontWeight: 'inherit',
-            },
-            'code::before': false,
-            'code::after': false,
-            code: {
-              backgroundColor: theme('colors.gray.200'),
-              padding: '.25em',
-            },
-          },
+          ],
         },
         light: {
           css: [
